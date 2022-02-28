cf361a1fa93741c81a9bc1b0186554c45d6a6c9d
ChrisMarSilva <chris.mar.silva@gmail.com>
2022-02-28 00:09:35 +0000

    ajustes

--- src/config/config_logging.py
+++ src/config/config_logging.py
@@ -107,4 +107,51 @@ def setup_logging() -> None:
     dictConfig(data)
     
     
+
+
+
+import logging
+import sys
+import typing as t
+
+from werkzeug.local import LocalProxy
+
+from .globals import request
+
+if t.TYPE_CHECKING:
+    from .app import Flask
+
+
+@LocalProxy
+def wsgi_errors_stream() -> t.TextIO:
+    return request.environ["wsgi.errors"] if request else sys.stderr
+
+
+def has_level_handler(logger: logging.Logger) -> bool:
+    level = logger.getEffectiveLevel()
+    current = logger
+    while current:
+        if any(handler.level <= level for handler in current.handlers):
+            return True
+        if not current.propagate:
+            break
+        current = current.parent  # type: ignore
+    return False
+
+default_handler = logging.StreamHandler(wsgi_errors_stream)  # type: ignore
+default_handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s"))
+
+def create_logger(app: "Flask") -> logging.Logger:.logging.wsgi_errors_stream` with a basic format.
+    """
+    logger = logging.getLogger(app.name)
+
+    if app.debug and not logger.level:
+        logger.setLevel(logging.DEBUG)
+
+    if not has_level_handler(logger):
+        logger.addHandler(default_handler)
+
+    return logger
+
+    
 '''
